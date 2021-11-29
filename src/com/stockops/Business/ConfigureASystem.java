7a6f459c6598fbb8b80e14f27555a00e1ebb98b5
Mridul Regmi <regmi.mr@northeastern.edu>
2021-11-28 22:39:43 -0500

    Skeleton code

---
+++ src/com/stockops/Business/ConfigureASystem.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stockops.Business;
+
+/**
+ *
+ * @author mridulregmi
+ */
+public class ConfigureASystem {
+
+    public static EcoSystem configure(){
+        
+        EcoSystem system = new EcoSystem();
+        
+        //Create a network
+        //create an enterprise
+        //initialize some organizations
+        //have some employees 
+        //create user account
+        
+        
+//        Employee employee = system.getEmployeeDirectory().createEmployee("RRH");
+//        
+//        system.setCustomerDirectory(new CustomerDirectory());
+//        system.setDeliveryManDirectory(new DeliveryManDirectory());
+//        system.setRestaurantDirectory(new RestaurantDirectory());
+//        system.setManagerDirectory(new ManagerDirectory());
+//        
+//        UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", employee, new SystemAdminRole());
+//        
+        return system;
+    }
+    
+}
+
